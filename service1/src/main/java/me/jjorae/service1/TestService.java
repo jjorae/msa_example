57ad67a80f7abd13bbdfd37ecfa7dfc0b4a1ff26
rhcho <rhcho@hancom.com>
2021-07-08 12:46:10 +0900

    config 서버 통해서 github 설정값 가져오는 부분까지 완료.

--- service1/src/main/java/me/jjorae/service1/TestService.java
+++ service1/src/main/java/me/jjorae/service1/TestService.java
@@ -1,7 +1,16 @@
 package me.jjorae.service1;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope // 이 어노테이션이 있어야 /actuator/refresh 할 때 바뀐 설정 값을 가져옴
 public class TestService {
+    @Value("${service1.msg}")
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
 }
--- service1/src/main/java/me/jjorae/service1/controller/TestController.java
+++ service1/src/main/java/me/jjorae/service1/controller/TestController.java
@@ -1,5 +1,6 @@
 package me.jjorae.service1.controller;
 
+import me.jjorae.service1.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    private final TestService testService;
+
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
     @GetMapping
     public String index() {
-        return "index";
+        return testService.getMsg();
     }
 }
